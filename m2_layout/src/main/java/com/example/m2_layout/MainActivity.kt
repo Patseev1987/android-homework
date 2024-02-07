cd810b9feb44256d07536541d663c6bad7c0d619
Patseev1987 <Patseev1987@gmal.com>
2024-02-07 16:28:41 +0300

    add m2_layout homework

--- m2_layout/src/main/java/com/example/m2_layout/MainActivity.kt
+++ m2_layout/src/main/java/com/example/m2_layout/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.m2_layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.example.m2_layout.databinding.ActivityMainBinding
-import com.example.m2_layout.databinding.BogdanCustomViewgroupBinding
 
 
 class MainActivity : AppCompatActivity() {
