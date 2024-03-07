b85cc999d03c8db11a9002867eb9dc18b43fd181
Patseev1987 <Patseev1987@gmal.com>
2024-03-07 10:02:34 +0300

    add m9 homework

--- m9_quiz_localization/src/main/java/com/example/m9_quiz_localization/StartFragment.kt
+++ m9_quiz_localization/src/main/java/com/example/m9_quiz_localization/StartFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.example.m9_quiz_localization.databinding.FragmentStartBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 
 class StartFragment : Fragment() {
@@ -30,6 +35,37 @@ class StartFragment : Fragment() {
         binding.bLetsGo.setOnClickListener {
             findNavController().navigate(R.id.action_startFragment_to_quizFragment)
         }
+
+        binding.bSetDataOfBirth.setOnClickListener {
+            setDataPicker(binding.bSetDataOfBirth)
+        }
+    }
+
+    private fun setDataPicker(view: View) {
+        activity?.supportFragmentManager?.let {
+
+            val dataPicker = MaterialDatePicker.Builder
+                .datePicker()
+                .setTitleText(R.string.set_your_data_oof_birth)
+                .build()
+            dataPicker.addOnPositiveButtonClickListener { timeInMillis ->
+                showSnackBarWithDataOfBirth(timeInMillis, view)
+            }
+            dataPicker.show(it, getString(R.string.set_your_data_oof_birth))
+        }
+    }
+
+    private fun showSnackBarWithDataOfBirth(timeInMillis: Long, view: View) {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = timeInMillis
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy")
+        val date = calendar.time
+        val dateForSnackBar = dateFormat.format(date)
+        Snackbar.make(
+            view,
+            getString(R.string.your_date_of_birth_is, dateForSnackBar),
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 
     override fun onDestroy() {
