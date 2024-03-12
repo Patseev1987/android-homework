2985f1704568d5653a6a5d4a3865a79809864a86
Bogdan Patseev <patseev1987@gmail.com>
2024-03-12 21:02:43 +0300

    add m12 homework

--- m12_mvvm/src/main/java/com/example/m12_mvvm/MainActivity.kt
+++ m12_mvvm/src/main/java/com/example/m12_mvvm/MainActivity.kt
@@ -3,33 +3,56 @@ package com.example.m12_mvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.m12_mvvm.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel:MainActivityViewModel
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        viewModel.setLength(binding.inputEditText.length())
+        observe(viewModel = viewModel, binding = binding)
+        setListeners(viewModel = viewModel, binding = binding)
+    }
 
+
+    private fun setListeners(binding: ActivityMainBinding, viewModel: MainActivityViewModel) {
+        binding.buttonSearch.setOnClickListener {
+            viewModel.loading()
+            viewModel.setRequest(binding.inputEditText.text.toString())
+        }
+        binding.inputEditText.doAfterTextChanged {
+            viewModel.setLength(binding.inputEditText.length())
+        }
+    }
+
+
+    private fun observe(binding: ActivityMainBinding, viewModel: MainActivityViewModel) {
+        viewModel.result.observe(this, Observer {
+            binding.results.text = it
+        })
         viewModel.isLoading.observe(this, Observer {
             if (it) {
                 binding.progressBar.visibility = View.VISIBLE
-            }else {
+                binding.buttonSearch.isEnabled = false
+                binding.inputEditText.isEnabled = false
+            } else {
                 binding.progressBar.visibility = View.GONE
+                binding.buttonSearch.isEnabled = true
+                binding.inputEditText.isEnabled = true
             }
         })
 
-        binding.buttonSearch.setOnClickListener {
-            viewModel.loading()
-        }
-
-
-
+        viewModel.length.observe(this, Observer {
+            binding.buttonSearch.isEnabled = it > 3
+        })
     }
 }
--- m12_mvvm/src/main/java/com/example/m12_mvvm/MainActivityViewModel.kt
+++ m12_mvvm/src/main/java/com/example/m12_mvvm/MainActivityViewModel.kt
@@ -7,21 +7,34 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.viewModelFactory
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
-class MainActivityViewModel:ViewModel() {
+class MainActivityViewModel : ViewModel() {
 
-    private var _isLoading:MutableLiveData<Boolean> = MutableLiveData(false)
+    private var _isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
     val isLoading: LiveData<Boolean> get() = _isLoading
-    private var results = listOf("We don't have any results!", "Not found!", "Connection error!")
-    private var _result:
+    private var request:String = ""
+    private var results = listOf("""We couldn't find anything about "%s"""", """"%s" - Not found!""", "Connection error!")
+    private var _result: MutableLiveData<String> = MutableLiveData()
+    val result: LiveData<String> get() = _result
+    private var _length: MutableLiveData<Int> = MutableLiveData()
+    val length: LiveData<Int> get() = _length
+
 
     private val scope = viewModelScope
-    fun loading(){
+    fun loading() {
         scope.launch {
             _isLoading.value = true
             delay(5000)
-
+            _result.value =  String.format( results[Random.nextInt(results.size)],request)
+            _isLoading.value = false
         }
+    }
 
+    fun setRequest(request:String){
+        this.request = request
+    }
+    fun setLength(length: Int) {
+        _length.value = length
     }
 }
